21fb3094889c58ad86fceb141ef45e94d52d7cff
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-07 12:13:46 +0100

    Update apiRequest.js

--- Immobiliersite/src/lib/apiRequest.js
+++ Immobiliersite/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://immobilier-api.onrender.com/api",
   withCredentials: true,
 });
 
