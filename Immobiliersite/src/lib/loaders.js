6c74ccad6cf176ffb363ed263877897c733aee76
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-08 14:34:24 +0100

    Update loaders.js

--- Immobiliersite/src/lib/loaders.js
+++ Immobiliersite/src/lib/loaders.js
@@ -2,34 +2,26 @@ import { defer } from "react-router-dom";
 import axiosInstance from "../axiosInstance/axios";
 
 export const singlePageLoader = async ({ request, params }) => {
-  const res = await axiosInstance("/posts/" + params.id);
+  const res = await axiosInstance.get("/posts/" + params.id);
   return res.data;
 };
+
 export const listPageLoader = async ({ request, params }) => {
-  const query = request.url.split("?")[1];
-  const postPromise = axiosInstance("/posts?" + query);
+  const query = request.url.split("?")[1] || "";
+  const postPromise = axiosInstance.get("/posts?" + query);
   return defer({
     postResponse: postPromise,
   });
 };
 
-//export const profilePageLoader = async () => {
- // const postPromise = axiosInstance("/users/profilePosts");
- // const chatPromise = axiosInstance("/chats");
-//  return defer({
-//    postResponse: postPromise,
-//    chatResponse: chatPromise,
-//  });
-//};
-
 export async function profileLoader() {
-  const userPostsPromise  = axios.get("https://immobilier-api.onrender.com/api/posts/user");
-  const savedPostsPromise = axios.get("https://immobilier-api.onrender.com/api/posts/saved");
-  const chatPromise       = axios.get("https://immobilier-api.onrender.com/api/chats");
+  const userPostsPromise  = axiosInstance.get("/posts/user");
+  const savedPostsPromise = axiosInstance.get("/posts/saved");
+  const chatPostsPromise  = axiosInstance.get("/chats");
 
   return defer({
     userPosts:  userPostsPromise,
     savedPosts: savedPostsPromise,
-    chatPosts:  chatPromise,
+    chatPosts:  chatPostsPromise,
   });
 }
