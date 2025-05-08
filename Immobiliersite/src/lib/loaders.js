1720909224b09f954fbb1c9da15be99d2c402e26
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-08 16:09:24 +0100

    Update loaders.js

--- Immobiliersite/src/lib/loaders.js
+++ Immobiliersite/src/lib/loaders.js
@@ -13,12 +13,16 @@ export const listPageLoader = async ({ request, params }) => {
   });
 };
 
-export const profilePageLoader = async () => {
-  const postPromise = axiosInstance("/users/profilePosts");
-  const chatPromise = axiosInstance("/chats");
+
+export const profilePageLoader = () => {
+  const userPostsPromise  = axiosInstance("/users/profilePosts");
+  const savedPostsPromise = axiosInstance("/users/savedPosts");
+  const chatPromise       = axiosInstance("/chats");
+
   return defer({
-    postResponse: postPromise,
-    chatResponse: chatPromise,
+    userPostsResponse:  userPostsPromise,
+    savedPostsResponse: savedPostsPromise,
+    chatResponse:       chatPromise,
   });
 };
 
