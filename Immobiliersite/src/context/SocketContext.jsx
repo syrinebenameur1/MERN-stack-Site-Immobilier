2fb8718ac21158972728ef87b4b9b74753dd3338
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-08 15:41:14 +0100

    Update SocketContext.jsx

--- Immobiliersite/src/context/SocketContext.jsx
+++ Immobiliersite/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("https://immobilier-api.onrender.com"));
+    setSocket(io("https://immobilier-socket.onrender.com"));
   }, []);
 
   useEffect(() => {
