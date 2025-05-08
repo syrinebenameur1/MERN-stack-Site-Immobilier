af8268efcc6192c27d180e74214d2ea6fc634aba
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-08 14:28:03 +0100

    Update profilePage.jsx

--- Immobiliersite/src/routes/profilePage/profilePage.jsx
+++ Immobiliersite/src/routes/profilePage/profilePage.jsx
@@ -56,25 +56,23 @@ console.log(err)
               <button>Create New Post</button>
             </Link>
           </div>
+
+          
           <Suspense fallback={<p>Loading...</p>}>
-            <Await
-              resolve={data.postResponse}
-              errorElement={<p>Error loading posts!</p>}
-            >
-              {(postResponse) => <List posts={postResponse.data.userPosts} />}
-            </Await>
+    <Await resolve={data.userPosts}>
+     {res => <List posts={res.data.userPosts} />}
+   </Await>
           </Suspense>
           <div className="title">
             <h1>Saved List</h1>
           </div>
           <Suspense fallback={<p>Loading...</p>}>
-            <Await
-              resolve={data.postResponse}
-              errorElement={<p>Error loading posts!</p>}
-            >
-              {(postResponse) => <List posts={postResponse.data.savedPosts} />}
-            </Await>
+    <Await resolve={data.savedPosts}>
+      {res => <List posts={res.data.savedPosts} />}
++   </Await>
           </Suspense>
+
+          
         </div>
       </div>
       <div className="chatContainer">
@@ -87,6 +85,9 @@ console.log(err)
               {(chatResponse) => <Chat chats={chatResponse.data}/>}
             </Await>
           </Suspense>
+
+
+          
         </div>
       </div>
     </div>
