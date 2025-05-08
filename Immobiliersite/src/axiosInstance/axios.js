6e2167f569d405e9742ef10db03c6249b821e350
Syrinee <149027410+syrinebenameur1@users.noreply.github.com>
2025-05-08 15:04:28 +0100

    Update axios.js

--- Immobiliersite/src/axiosInstance/axios.js
+++ Immobiliersite/src/axiosInstance/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://immobilier-api.onrender.com/api',
+  baseURL: 'https://immobilier-api.onrender.com',
   withCredentials: true, // Ensures cookies are sent in requests
   headers: {
     'Content-Type': 'application/json',
